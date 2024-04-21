f3f42edc2adbe718c1098eb656e5c3964a906d43
Wong, Dylan <dylanwong@nevada.unr.edu>
2024-04-21 09:04:03 -0700

    working on configurator UI

--- src/main/java/gregtech/common/items/behaviors/DataConfiguratorBehavior.java
+++ src/main/java/gregtech/common/items/behaviors/DataConfiguratorBehavior.java
@@ -1,5 +1,6 @@
 package gregtech.common.items.behaviors;
 
+import gregtech.api.block.machines.MachineItemBlock;
 import gregtech.api.items.gui.ItemUIFactory;
 import gregtech.api.items.metaitem.MetaItem;
 import gregtech.api.items.metaitem.stats.IDataItem;
@@ -7,33 +8,34 @@ import gregtech.api.items.metaitem.stats.IItemBehaviour;
 import gregtech.api.mui.GTGuiTextures;
 import gregtech.api.mui.GTGuis;
 import gregtech.api.mui.factory.MetaItemGuiFactory;
-
 import gregtech.api.recipes.CompoundRecipe;
-
-import gregtech.api.util.AdvancedProcessingLineManager;
-
-import gregtech.api.util.AssemblyLineManager;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.items.ItemStackHandler;
 
 import com.cleanroommc.modularui.api.drawable.IKey;
-import com.cleanroommc.modularui.drawable.ItemDrawable;
 import com.cleanroommc.modularui.factory.HandGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Alignment;
 import com.cleanroommc.modularui.value.sync.GuiSyncManager;
 import com.cleanroommc.modularui.value.sync.SyncHandlers;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.ItemSlot;
 import com.cleanroommc.modularui.widgets.layout.Column;
 import com.cleanroommc.modularui.widgets.layout.Row;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataConfiguratorBehavior implements IItemBehaviour, ItemUIFactory {
 
@@ -58,59 +60,40 @@ public class DataConfiguratorBehavior implements IItemBehaviour, ItemUIFactory {
         var column = new Column().top(5).margin(7, 0)
                 .widthRel(1f).coverChildrenHeight();
         column.child(new Row()
-//                .pos(4, 4)
+                //                .pos(4, 4)
                 .height(16).coverChildrenWidth()
-                .child(new ItemDrawable(guiData.getUsedItemStack()).asWidget().size(16).marginRight(4))
                 .child(IKey.str(guiData.getUsedItemStack().getDisplayName()).color(0xFF222222).asWidget().heightRel(1.0f)));
+
         column.child(new Row().coverChildrenHeight().marginBottom(2).widthRel(1f)
                 .child(new ItemSlot()
-                        .slot(SyncHandlers.itemSlot(guiData.isClient() ? clientStackhandler : serverStackHandler, 0)
-                                .filter(DataConfiguratorBehavior::isItemValid)
+                        .slot(SyncHandlers.itemSlot(getStackHandler(guiData), 0)
+                                .filter(DataConfiguratorBehavior::isDataItem)
                                 .singletonSlotGroup(101))
                         .size(18).marginRight(2)
-                        .background(GTGuiTextures.SLOT, GTGuiTextures.DATA_ORB_OVERLAY.asIcon().size(16))));
+                        .background(GTGuiTextures.SLOT, GTGuiTextures.DATA_ORB_OVERLAY.asIcon().size(16)))
+                .child(new ItemSlot()
+                        .slot(SyncHandlers.phantomItemSlot(getStackHandler(guiData), 1)
+                                .filter(DataConfiguratorBehavior::isMachineItem)
+                                .singletonSlotGroup(101))
+                        .setEnabledIf(widget -> hasDataItem(guiData))
+                )
+//                .child(IKey.dynamic(() -> I18n.format("recipemap." + getRecipeMap(selectedMapIndex.getIntValue()).unlocalizedName + ".name")).color(0xFF222222).asWidget()
+//                        .heightRel(1.0f)
+//                        .setEnabledIf(widget -> hasDataItem(guiData))
+//                )
+                .child(new ButtonWidget<>()
+                        .size(18)
+                        .background(GTGuiTextures.BUTTON)
+                        .align(Alignment.TopRight)
+                        .setEnabledIf(widget -> hasDataItem(guiData))
+                        .onMouseReleased(button -> {
+                            return true;
+                        }
+                )));
         return panel.child(column).bindPlayerInventory();
     }
 
-    @Nullable
-    private CompoundRecipe readCompoundRecipe() {
-        return AdvancedProcessingLineManager.readCompoundRecipe(getStoredStack());
-    }
-
-    private boolean writeCompoundRecipe(CompoundRecipe recipe) {
-        ItemStack dataStack = getStoredStack();
-        if (dataStack == ItemStack.EMPTY) return false;
-        NBTTagCompound tag = new NBTTagCompound();
-        AdvancedProcessingLineManager.writeCompoundRecipeToNBT(tag, recipe);
-        dataStack.setTagCompound(tag);
-        return true;
-    }
-
-    private int getDataStackComplexityLimit(ItemStack dataStack) {
-        int max = 0;
-        if (dataStack.getItem() instanceof MetaItem<?> metaItem) {
-            for (IItemBehaviour behaviour : metaItem.getBehaviours(dataStack)) {
-                if (behaviour instanceof IDataItem dataBehavior) {
-                    max = Math.max(max, dataBehavior.maxCompoundRecipeComplexity());
-                }
-            }
-        }
-        return max;
-    }
-
-    private boolean canManipulateDataStack(ItemStack dataStack) {
-        NBTTagCompound tag = dataStack.getTagCompound();
-        return tag == null || !tag.hasKey(AssemblyLineManager.RESEARCH_NBT_TAG);
-    }
-
-    private ItemStack getStoredStack() {
-        ItemStack dataStack = serverStackHandler.getStackInSlot(0);
-        if (serverStackHandler.getStackInSlot(0) == ItemStack.EMPTY)
-            dataStack = clientStackhandler.getStackInSlot(0);
-        return dataStack;
-    }
-
-    protected static boolean isItemValid(ItemStack stack) {
+    protected static boolean isDataItem(ItemStack stack) {
         if (stack.getItem() instanceof MetaItem<?> metaItem) {
             for (IItemBehaviour behaviour : metaItem.getBehaviours(stack)) {
                 if (behaviour instanceof IDataItem) {
@@ -121,9 +104,30 @@ public class DataConfiguratorBehavior implements IItemBehaviour, ItemUIFactory {
         return false;
     }
 
+    protected static boolean isMachineItem(ItemStack stack) {
+        return stack.getItem() instanceof MachineItemBlock;
+    }
+
+    private DataStackHandler getStackHandler(HandGuiData guiData) {
+        return guiData.isClient() ? clientStackhandler : serverStackHandler;
+    }
+
+    private boolean hasDataItem(HandGuiData guiData) {
+        return !getStackHandler(guiData).getStackInSlot(0).isEmpty();
+    }
+
+//    private RecipeMap<SimpleRecipeBuilder> getRecipeMap(HandGuiData guiData) {
+//        if (getStackHandler(guiData).getStackInSlot(1).getItem() instanceof MachineItemBlock) {
+//            getStackHandler(guiData).getStackInSlot(1).getItem()
+//        }
+//    }
+
     protected static class DataStackHandler extends ItemStackHandler {
-        protected DataStackHandler() {
-            super();
+
+        public final List<CompoundRecipe.RecipeInfo> recipes = new ArrayList<>();
+
+        public DataStackHandler() {
+            super(2);
         }
 
         @Override
@@ -133,7 +137,30 @@ public class DataConfiguratorBehavior implements IItemBehaviour, ItemUIFactory {
 
         @Override
         public boolean isItemValid(int slot, @NotNull ItemStack stack) {
-            return DataConfiguratorBehavior.isItemValid(stack);
+            return (slot == 0)? DataConfiguratorBehavior.isDataItem(stack) : DataConfiguratorBehavior.isMachineItem(stack);
+        }
+
+        @Override
+        protected void onContentsChanged(int slot) {
+            if (slot == 0 && !getStackInSlot(slot).isEmpty()) {
+                syncDataItemNBT(getStackInSlot(slot));
+            }
+            super.onContentsChanged(slot);
+        }
+
+        private void syncDataItemNBT(ItemStack stack) {
+            NBTTagCompound tag = stack.getTagCompound();
+            if (tag == null) {
+                tag = new NBTTagCompound();
+                tag.setTag("recipes", new NBTTagList());
+                stack.setTagCompound(tag);
+            }
+            NBTTagList nbtRecipes = tag.getTagList("recipes", Constants.NBT.TAG_LIST);
+            for (NBTBase nbtRecipe : nbtRecipes) {
+                if (nbtRecipe instanceof NBTTagCompound) {
+                    recipes.add(CompoundRecipe.RecipeInfo.readFromNBT((NBTTagCompound) nbtRecipe));
+                }
+            }
         }
     }
 
